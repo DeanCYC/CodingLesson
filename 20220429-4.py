1185d58dfe8ab2e59dd8b8563e8e124ec1f40d3d
Dean Chen <contactchenyuchen@gmail.com>
2022-04-29 15:59:48 +0900

    Add HW8

---
+++ 20220429-4.py
@@ -0,0 +1,17 @@
+#HW8 Mad libs game
+#A game where you can enter in a bunch of random words
+print("Roses are red")
+print("Violets are blue")
+print("I love you")
+print("\n")
+
+#create varibles
+#print("Roses are {color}")
+#print("{plural_noun} are blue")
+#print("I love {celebrity}")
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
