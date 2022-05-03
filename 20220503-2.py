458d4cceb99db1500ae2904427cc74a982535082
Dean Chen <contactchenyuchen@gmail.com>
2022-05-03 22:32:45 +0900

    Add HW14-2

---
+++ 20220503-2.py
@@ -0,0 +1,15 @@
+#HW14-2 If statements
+is_male = True #create a boolean variable
+#can use if statement to cheak to see what the value of this variable is and if it's true then can do something
+if is_male: #the value has to be able to be reducible to a true or false value
+    #anything that put below this with and indentation is going to be executed when that conditions true
+    print("You are a male")
+#change is_male = False 
+#then won't print out anything
+print("\n")
+
+is_male2 = False
+if is_male2: 
+    print("You are a male")
+else:
+    print("You are not a male")
