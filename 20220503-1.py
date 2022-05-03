305d297f8743f661eb8616f4cf39685c4fc8c94f
Dean Chen <contactchenyuchen@gmail.com>
2022-05-03 22:13:44 +0900

    Updated HW14-1

--- 20220503-1.py
+++ 20220503-1.py
@@ -1,3 +1,23 @@
-#HW14 If statements
+#HW14-1 If statements - explanation
 #if statements = allow the programs to respond to the input that they are given
+#Case A
+#I wake up
+#If I'm hungry # It's a condition
+    #I eat breakfast #If condition is ture, then do this
+    #If condition is false, then move on (because only one condition)
 
+#Case B
+#I leave my house
+#If it's cloudy #Condition
+    #I bring an umbrella #If the condition is true, then do this.
+#otherwise
+    #I bring sunglasses #If the condition is false, then do this.
+
+#Case C
+#I'm at a restaurant
+#If I want meat #Condition A
+    #I order a steak #If the condition A is true, then do this.
+#otherwise if I want pasta #Check condition B when condition A is false
+    #I order spaghetti & meatballs #If the condition B is true, then do this.
+#otherwise 
+    #I order a salad #If condition B is false, then do this.
