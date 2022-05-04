2c1f7a620ae8fa2dcf5f6440ab03ba8ed2f37517
Dean Chen <contactchenyuchen@gmail.com>
2022-05-04 23:53:33 +0900

    updated file

--- 20220504-1.py
+++ 20220504-1.py
@@ -1,1 +1,9 @@
-#adding infomation
+#if statements
+is_male = True #create a boolean variable
+#can use if statement to cheak to see what the value of this variable is and if it's true then can do something
+if is_male: #the value has to be able to be reducible to a true or false value
+    #anything that put below this with and indentation is going to be executed when that conditions true
+    print("You are a male")
+#change is_male = False 
+#then won't print out anything
+print("\n")
