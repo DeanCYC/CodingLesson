88d9456e74471d1851bcde570a001b238315555e
Dean Chen <contactchenyuchen@gmail.com>
2022-05-05 23:24:38 +0900

    Updated HW15

--- 20220504-1.py
+++ 20220504-1.py
@@ -1,9 +1,17 @@
 #HW15 If statements & comparison
 def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3: #>= is one of comparison operators
+    if num1 >= num2 and num1 >= num3: 
         return num1
     elif num2 >= num1 and num2 >= num3:
         return num2
     else:
         return num3
 print(max_num(300,40,5))
+
+#comparsion operators
+#>= great than or equal to
+#== two values are equal
+#!= not equal
+#> great than
+#< less than
+#<= less than or equal to
