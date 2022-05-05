037d3e53ac69bf941c5cef5a49aa74fd2e911220
Dean Chen <contactchenyuchen@gmail.com>
2022-05-05 23:20:50 +0900

    Updated HW15

--- 20220504-1.py
+++ 20220504-1.py
@@ -1,9 +1,9 @@
-#if statements
-is_male = True #create a boolean variable
-#can use if statement to cheak to see what the value of this variable is and if it's true then can do something
-if is_male: #the value has to be able to be reducible to a true or false value
-    #anything that put below this with and indentation is going to be executed when that conditions true
-    print("You are a male")
-#change is_male = False 
-#then won't print out anything
-print("\n")
+#HW15 If statements & comparison
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3: #>= is one of comparison operators
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+print(max_num(300,40,5))
