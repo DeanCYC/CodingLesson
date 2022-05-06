5453b401755f7268d4234d024bddf801c8f46b1d
Dean Chen <contactchenyuchen@gmail.com>
2022-05-06 23:50:44 +0900

    Updated HW17

--- 20220506-1.py
+++ 20220506-1.py
@@ -24,4 +24,3 @@ print("\n")
 print(monthConversions.get("Dec")) 
 print(monthConversions.get("Luv")) 
 print(monthConversions.get("Luv", "Not a valid Key")) #can specify a default value if the key is not found
-print("\n")
