8938d25e101006f8a2fe53a14e8fa7041ecda636
Dean Chen <contactchenyuchen@gmail.com>
2022-05-07 23:40:20 +0900

    Add HW18

---
+++ 20220507-1.py
@@ -0,0 +1,13 @@
+#HW18 While loop
+#allow us to loop though and execute a block of code multiple times
+i = 1
+while i <=10:  #condition i ＝ raise by Japanese 
+    print(i)
+    i += 1
+
+print("Don't with loop")
+
+#1. check the condition first time
+#2. loop though all the code
+#3. go back up to the top and check it again
+#4. once the condition is not true, then come out from while loop
