14d0959654f92879eacd1ccbfe597380c8d325de
Dean Chen <contactchenyuchen@gmail.com>
2022-05-09 23:54:54 +0900

    updated HW20

--- 20220509-1.py
+++ 20220509-1.py
@@ -1,2 +1,6 @@
 #HW20 For loop
-    print("\n")
+#allow to loop over a different collections of items
+#specify a variable to be used on every iteration of for loop 
+#and each time most likely have a different value
+for letter in "Giraffe Academy": #for every letter inside of giraffe academy, will be done by somthing
+    print(letter) #looping though all of the letters inside of giraffe academy
