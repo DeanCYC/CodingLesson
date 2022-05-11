e11cba99364a175e879400f0d1a743428aea31b7
Dean Chen <contactchenyuchen@gmail.com>
2022-05-11 12:16:57 +0900

    Add HW24

---
+++ 20220511-3.py
@@ -0,0 +1,9 @@
+#HW24 Comments
+print("Comment are fun")
+#a comment is a line inside of a python file that's not going to get rendered by Python
+#Method:
+#1. # + comment (recommend)
+#2. ’‘’ ＋ ‘’‘’
+#Purpose:
+#1. Write a note
+#2. Comment out code for testing
