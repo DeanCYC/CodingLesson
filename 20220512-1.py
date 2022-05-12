5af0707b105307543b52ba0560ddb3d10617ba8a
Dean Chen <contactchenyuchen@gmail.com>
2022-05-12 16:52:58 +0900

    Add HW25-1

---
+++ 20220512-1.py
@@ -0,0 +1,8 @@
+#HW25-1 Try except
+#catching error in python
+number = int(input("Enter a number: "))
+print(number)
+print("\n")
+#enter "as"
+#result showed invalid literal for int() with base 10: 'as' means you didn't enter a number but word
+
