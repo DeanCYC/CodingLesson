869d32da8009bb035ee7d1502836d045465d9b9d
Dean Chen <contactchenyuchen@gmail.com>
2022-05-13 19:04:12 +0900

    Updated #HW26-3

--- 20220513-3.py
+++ 20220513-3.py
@@ -2,8 +2,6 @@
 employee_file = open("employees.text", "r")
 #3. read individual line inside the file
 print(employee_file.readlines()) #reading all lines and put inside a list
-print("\n")
-
 #print(employee_file.readlines()[1]) #access a specific line by index
 
 employee_file.close()
