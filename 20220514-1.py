a03cf4bce9c2282fc60815b5619efbe8a334a8bc
Dean Chen <contactchenyuchen@gmail.com>
2022-05-15 20:48:18 +0900

    Add HW27-1

--- 20220514-1.py
+++ 20220514-1.py
@@ -1,2 +1,10 @@
-#HW27 Writing files
-print("\n")
+#HW27-1 Writing files
+#Writing a new file and appending onto existing files
+
+#appending: adding will be permanently saved to the file
+employee_file = open("employees.text", "a")
+
+employee_file.write("Toby - Human Resources")
+employee_file.write("\nKelly - Customer Service") #\n + new info: make it shows in the new line
+
+employee_file.close()
