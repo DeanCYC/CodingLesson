a5e969e7a0ce42b8a59c5d2e8cbe438ed5c2e9c7
Dean Chen <contactchenyuchen@gmail.com>
2022-05-16 22:53:24 +0900

    Updated HW28-1

--- 20220516-1.py
+++ 20220516-1.py
@@ -5,3 +5,7 @@ import useful_tools #import file:useful_tools
 
 print(useful_tools.roll_dice(10)) #use the function from that file
 
+#check https://docs.python.org/3/py-modindex.html can find more functions
+#there are two types of models
+#1. built in modules (can automatically have access to them)
+#2. external modules (are stored in lib folder)
