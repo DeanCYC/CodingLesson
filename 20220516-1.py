314bc044212bce5b45c70f2a2615d4dd7926e2f4
Dean Chen <contactchenyuchen@gmail.com>
2022-05-16 22:20:14 +0900

    Add HW28-1

---
+++ 20220516-1.py
@@ -0,0 +1,7 @@
+#HW28-1 Modules and pip
+#module: a python file that can import into current python file
+
+import useful_tools #import file:useful_tools
+
+print(useful_tools.roll_dice(10)) #use the function from that file
+
