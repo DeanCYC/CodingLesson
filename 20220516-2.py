dd0ccaa99991bfeb46a1991fb3563cb3d19a3a19
Dean Chen <contactchenyuchen@gmail.com>
2022-05-16 22:53:31 +0900

    Add HW28-2

---
+++ 20220516-2.py
@@ -0,0 +1,15 @@
+#HW28-2 Modules and pip
+#pip: can use to install external modules that made by 3rd party
+#pip can be refered to a pabckage manager and only allow to install, manage, update, uninstall
+#if the version of Python is not lastest version, may need to install "pip" separately
+#ex. python.docx
+#1. open terminal
+#2. check version:
+pip --version
+#4. install external module that want to use via pip
+#5. input "pip install python-docx"
+#Most external modules are able to be install via pop
+#If can't, then will have some details installation instructions
+#Usually install 3rd party modules is going to put inside of Lib/site-packages folder
+#6. After installed, can use functions in that modules
+#7. uninstall: "pip uninstall python-docx"
