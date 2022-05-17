c3c3088c9752808547dd8e0b0f645dd40314df20
Dean Chen <contactchenyuchen@gmail.com>
2022-05-17 12:53:30 +0900

    Add file for 20220517

--- 20220517/App.py
+++ 20220517/App.py
@@ -5,5 +5,7 @@ from Student import Student #Referring to the file and referring to actual stude
 student1 = Student("Jim", "Business", 3.1, False) #create a student
 #class is like an overall template and it defiens what a student is
 #object is an instance of a class with actual information for a student
+student2 = Student("Pam", "Art", 2.5, True)
 
-print(student1.name)
+print(student1.name) #access the student's information
+print(student2.gpa)
--- 20220517/Student.py
+++ 20220517/Student.py
@@ -5,7 +5,13 @@
 class Student:
     #initialize function
     def __init__(self, name, major, gpa, is_on_probation): #define what info needed for the student data type
+        #self.name = an attribute of an object
         self.name = name
+        #the name of the student object is equal to the name that we passed in
         self.major = major
+        #the major of the student object is equal to the major that we passed in
         self.gpa = gpa
+        #the gpa of the student object is equal to the gpa that we passed in
         self.is_on_probation = is_on_probation
+
+#can use classes and objects to model anything Ex.phone
