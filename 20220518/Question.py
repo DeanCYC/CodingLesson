615f6e73a004629bd1315d6142a39acb17b8cfe3
Dean Chen <contactchenyuchen@gmail.com>
2022-05-19 00:44:46 +0900

    Updated HW30-2

--- 20220518/Question.py
+++ 20220518/Question.py
@@ -1,6 +1,6 @@
 #HW30-2 Building a multiple choice quiz
 #question class: able to store question prompts and answers
 class Question:
-    def _init_(self, prompt, answer):
+    def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
