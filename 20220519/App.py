bbb52ca00e83de43361f68acea558683d9e231c4
Dean Chen <contactchenyuchen@gmail.com>
2022-05-19 01:04:33 +0900

    Add HW31-1

---
+++ 20220519/App.py
@@ -0,0 +1,11 @@
+#HW31-1 Object Functions
+#class function in Python: a function can be used in a class
+#1. modify the objects of that class
+#2. give a specific informatin about the objects
+
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 = Student("Phyllis", "Business", 3.8)
+
+print(student2.on_honor_ro11)
