31a3a58643305c4e6ab5c7258098cc70b34896ff
Dean Chen <contactchenyuchen@gmail.com>
2022-05-19 00:29:30 +0900

    Updated HW30-1

---
+++ 20220518/App.py
@@ -0,0 +1,24 @@
+#HW30-1 Building a multiple choice quiz
+from Question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Geen\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"), #create question object and assign the answer
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b"),
+]
+
+def run_test(questions): #create question array
+    score = 0
+    for question in questions: #for each question object inside of the question array
+        answer = input(question.prompt)
+        if answer == question.answer: #check answer that input by user is euqal to the answer of current question
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " Correct")
+
+run_test(questions)
--- 20220518/20220518-1.py
+++
@@ -1,7 +0,0 @@
-#HW30 Building a multiple choice quiz
-
-question_prompts = [
-    "What color are apples?\n(a) Red/Geen\n(b) Purple\n(c) Orange\n\n",
-    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
-]
