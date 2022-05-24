dfb21a23677c366a9dc6da2125645ee39bd66c19
Dean Chen <contactchenyuchen@gmail.com>
2022-05-24 21:03:00 +0900

    Add 20220524-2

---
+++ 20220524/20220524-2.py
@@ -0,0 +1,18 @@
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))    # <class 'int'>
+print(type(b))    # <class 'float'>
+print(type(c))    # <class 'complex'>
+print(type(d))    # <class 'str'>
+print(type(e))    # <class 'bool'>
+
+'''
+int()：將一個數值或字符串轉換成整數，可以指定進制。
+float()：將一個字符串轉換成浮點數。
+str()：將指定的對象轉換成字符串形式，可以指定編碼。
+chr()：將整數轉換成該編碼對應的字符串（一個字符）。
+ord()：將字符串（一個字符）轉換成對應的編碼（整數）。
+'''
