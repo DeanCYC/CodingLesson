68a194abe4886285a506b96feee8718b0bb74e82
Dean Chen <contactchenyuchen@gmail.com>
2022-05-25 21:39:25 +0900

    Add 20220525-5

---
+++ 20220525/20220525-5.py
@@ -0,0 +1,13 @@
+#百分制成績換算成等級制成績
+score = float(input('請輸入成績: '))
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'E'
+print('對應的等級是:', grade)
