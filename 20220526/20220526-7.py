d4e236169c382c99bd31bf671784b70468a0cffc
Dean Chen <contactchenyuchen@gmail.com>
2022-05-26 23:22:45 +0900

    Add 20220526-7

---
+++ 20220526/20220526-7.py
@@ -0,0 +1,13 @@
+#輸入兩個正整數計算它們的最大公約數和最小公倍數
+x = int(input('x = '))
+y = int(input('y = '))
+# 如果x大於y就交換x和y的值
+if x > y:
+    # 通過下面的操作將y的值賦給x, 將x的值賦給y
+    x, y = y, x
+# 從兩個數中較小的數開始做遞減的循環
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公約數是%d' % (x, y, factor))
+        print('%d和%d的最小公倍數是%d' % (x, y, x * y // factor))
+        break
