d48219f6e70e18323114aeb419aafb27a5157b3c
Dean Chen <contactchenyuchen@gmail.com>
2022-05-30 18:13:17 +0900

    Add 20220529-10

---
+++ 20220529/20220529-10.py
@@ -0,0 +1,13 @@
+fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+fruits += ['pitaya', 'pear', 'mango']
+# 列表切片
+fruits2 = fruits[1:4]
+print(fruits2) # apple strawberry waxberry
+# 可以通過完整切片操作來複製列表
+fruits3 = fruits[:]
+print(fruits3) # ['grape', 'apple', 'strawberry', 'waxberry', 'pitaya', 'pear', 'mango']
+fruits4 = fruits[-3:-1]
+print(fruits4) # ['pitaya', 'pear']
+# 可以通過反向切片操作來獲得倒轉後的列表的拷貝
+fruits5 = fruits[::-1]
+print(fruits5) # ['mango', 'pear', 'pitaya', 'waxberry', 'strawberry', 'apple', 'grape']
