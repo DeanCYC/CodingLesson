12ed1190f7e25e9ea76b7b50e70433edb1b11d93
Dean Chen <contactchenyuchen@gmail.com>
2022-05-30 18:13:12 +0900

    Add 20220529-9

---
+++ 20220529/20220529-9.py
@@ -0,0 +1,22 @@
+list1 = [1, 3, 5, 7, 100]
+# 添加元素
+list1.append(200)
+list1.insert(1, 400)
+# 合併兩個列表
+# list1.extend([1000, 2000])
+list1 += [1000, 2000]
+print(list1) # [1, 400, 3, 5, 7, 100, 200, 1000, 2000]
+print(len(list1)) # 9
+# 先通過成員運算判斷元素是否在列表中，如果存在就刪除該元素
+if 3 in list1:
+	list1.remove(3)
+if 1234 in list1:
+    list1.remove(1234)
+print(list1) # [1, 400, 5, 7, 100, 200, 1000, 2000]
+# 從指定的位置刪除元素
+list1.pop(0)
+list1.pop(len(list1) - 1)
+print(list1) # [400, 5, 7, 100, 200, 1000]
+# 清空列表元素
+list1.clear()
+print(list1) # []
