37cddf4dd8faddd42559ac98807a52a33b76dbaa
Dean Chen <contactchenyuchen@gmail.com>
2022-05-30 17:53:51 +0900

    Add 20220528-8

---
+++ 20220528/20220528-8.py
@@ -0,0 +1,18 @@
+def foo():
+    b = 'hello'
+
+    # Python中可以在函數內部再定義函數
+    def bar():
+        c = True
+        print(a)
+        print(b)
+        print(c)
+
+    bar()
+    # print(c)  # NameError: name 'c' is not defined
+
+
+if __name__ == '__main__':
+    a = 100
+    # print(b)  # NameError: name 'b' is not defined
+    foo()
