b0b51718fd8a02c005c76fd26de150a331ef8bd1
Dean Chen <contactchenyuchen@gmail.com>
2022-05-31 22:48:10 +0900

    Add 20220531-3

---
+++ 20220531/20220531-3.py
@@ -0,0 +1,44 @@
+#井字棋遊戲
+import os
+
+
+def print_board(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
+    print('-+-+-')
+    print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
+    print('-+-+-')
+    print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
+
+
+def main():
+    init_board = {
+        'TL': ' ', 'TM': ' ', 'TR': ' ',
+        'ML': ' ', 'MM': ' ', 'MR': ' ',
+        'BL': ' ', 'BM': ' ', 'BR': ' '
+    }
+    begin = True
+    while begin:
+        curr_board = init_board.copy()
+        begin = False
+        turn = 'x'
+        counter = 0
+        os.system('clear')
+        print_board(curr_board)
+        while counter < 9:
+            move = input('輪到%s走棋, 請輸入位置: ' % turn)
+            if curr_board[move] == ' ':
+                counter += 1
+                curr_board[move] = turn
+                if turn == 'x':
+                    turn = 'o'
+                else:
+                    turn = 'x'
+            os.system('clear')
+            print_board(curr_board)
+        choice = input('再玩一局?(yes|no)')
+        begin = choice == 'yes'
+
+
+if __name__ == '__main__':
+    main()
+#說明： 最後這個案例來自《Python編程快速上手:讓繁瑣工作自動化》一書（這本書對有編程基礎想迅速使用Python將日常工作自動化的人來說還是不錯的選擇），對代碼做了一點點的調整。
