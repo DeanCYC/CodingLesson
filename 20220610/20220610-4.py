e1d6916917b84115ff9fe9afbed9a6af1e906424
Dean Chen <contactchenyuchen@gmail.com>
2022-06-10 23:46:14 +0900

    Add 20220610-4

---
+++ 20220610/20220610-4.py
@@ -0,0 +1,45 @@
+"""
+遞歸回溯法：叫稱為試探法，按選優條件向前搜索，當搜索到某一步，發現原先選擇並不優或達不到目標時，就退回一步重新選擇，比較經典的問題包括騎士巡邏、八皇后和迷宮尋路等。
+"""
+import sys
+import time
+
+SIZE = 5
+total = 0
+
+
+def print_board(board):
+    for row in board:
+        for col in row:
+            print(str(col).center(4), end='')
+        print()
+
+
+def patrol(board, row, col, step=1):
+    if row >= 0 and row < SIZE and \
+        col >= 0 and col < SIZE and \
+        board[row][col] == 0:
+        board[row][col] = step
+        if step == SIZE * SIZE:
+            global total
+            total += 1
+            print(f'第{total}種走法: ')
+            print_board(board)
+        patrol(board, row - 2, col - 1, step + 1)
+        patrol(board, row - 1, col - 2, step + 1)
+        patrol(board, row + 1, col - 2, step + 1)
+        patrol(board, row + 2, col - 1, step + 1)
+        patrol(board, row + 2, col + 1, step + 1)
+        patrol(board, row + 1, col + 2, step + 1)
+        patrol(board, row - 1, col + 2, step + 1)
+        patrol(board, row - 2, col + 1, step + 1)
+        board[row][col] = 0
+
+
+def main():
+    board = [[0] * SIZE for _ in range(SIZE)]
+    patrol(board, SIZE - 1, SIZE - 1)
+
+
+if __name__ == '__main__':
+    main()
